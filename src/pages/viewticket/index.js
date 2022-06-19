0b7f06adaf7439b112cdc439c75d5d63443a6e23
sys <paytf@outlook.com>
2022-06-19 22:46:48 +0100

    <feat>: improved ticket look for pc

--- src/pages/viewticket/index.js
+++ src/pages/viewticket/index.js
@@ -7,14 +7,13 @@ import styles from "./viewticket.module.css";
 
 const ViewTicket = () => {
     const userData = JSON.parse(localStorage.getItem("userData"));
-    const { id: discordId, avatar, roles, username } = userData;
+    const { id: discordId, avatar, username } = userData;
     const profilePicture = `https://cdn.discordapp.com/avatars/${discordId}/${avatar}.webp?size=1024`;
-    const serverRoles = JSON.parse(localStorage.getItem("serverRoles"));
-    const playerRoles = serverRoles.filter((p) => roles.includes(p.roleId));
 
     const { id } = useParams();
     const [ticket, setTicket] = useState({});
     const [isFound, setIsFound] = useState(false);
+    const [sender, setSender] = useState({});
     useEffect(() => {
         axios.get(`http://45.146.254.137:3001/api/tickets/${id}`, {
             withCredentials: true,
@@ -22,7 +21,8 @@ const ViewTicket = () => {
             if (res.data) {
                 console.log(res.data)
                 setTicket(res.data);
-                setIsFound(true)
+                setSender(res.data.sender)
+                setIsFound(true);
             }
         }
         );
@@ -34,13 +34,12 @@ const ViewTicket = () => {
                     <ul className={styles.commentSection}>
                         <li className={`${styles.comment} ${styles.userComment}`}>
                             <div className={`${styles.contentContainer} ${styles.firstContainer}`}>
-                                <div className={styles.ticketSender}>
+                                <div className={styles.playerInfoContainer}>
                                     <div className={styles.playerInfo}>
-                                        <div className={styles.avatar} href="/">
-                                            <img src={profilePicture} width="64px" alt={username} title={username} />
+                                        <div className={styles.avatar}>
+                                            <img src={`https://cdn.discordapp.com/avatars/${sender.discordId}/${sender.avatar}.webp?size=1024`} width="64px" alt={sender.username} title={sender.username} />
                                         </div>
-                                        <span className={styles.playerInfoName} style={{ color: playerRoles[0].color }}>PlayerName</span>
-                                        <span className={styles.ticketStarter}>Starter</span>
+                                        <span className={styles.playerInfoName} style={{ color: 'var(--color-grass)' }}>{sender.username}</span>
                                     </div>
                                 </div>
                                 <p className={styles.commentContent}>{ticket.message}</p>
@@ -51,25 +50,30 @@ const ViewTicket = () => {
                                 reply.isStaff ?
                                     <li className={`${styles.comment} ${styles.staffComment}`} key={id}>
                                         <div className={styles.staffContentContainer}>
-                                            <div className={styles.ticketSender}>
+                                            <div className={styles.playerInfoContainer}>
                                                 <div className={styles.playerInfo}>
                                                     <div className={styles.avatar} href="/">
                                                         <img src={`https://cdn.discordapp.com/avatars/${reply.sender.discordId}/${reply.sender.avatar}.webp?size=1024`} width="64px" alt="Profile Avatar" title="Jack Smith" />
                                                     </div>
-                                                    <span className={styles.playerInfoName} style={{ color: playerRoles[0].color }}>PlayerName</span>
+                                                    <span className={styles.playerInfoName} style={{ color: 'var(--color-salmon)' }}>{reply.sender.username}</span>
                                                     <span className={styles.staffReplyInfo}>STAFF</span>
                                                 </div>
                                             </div>
-                                            <p className={styles.staffCommentContent}>{ticket.message}</p>
+                                            <p className={styles.staffCommentContent}>
+                                                Greetings <b>{sender.username},</b> <br /><br />
+                                                {ticket.message}<br /><br />
+                                                Sincerely.<br />
+                                                <b>Dz-Crew RolePlay Staff</b>
+                                            </p>
                                         </div>
                                     </li> : <li className={`${styles.comment} ${styles.userComment}`} key={id}>
                                         <div className={styles.contentContainer}>
-                                            <div>
+                                            <div className={styles.playerInfoContainer}>
                                                 <div className={styles.playerInfo}>
                                                     <div className={styles.avatar} href="/">
                                                         <img src={`https://cdn.discordapp.com/avatars/${reply.sender.discordId}/${reply.sender.avatar}.webp?size=1024`} width="64px" alt="Profile Avatar" title="Jack Smith" />
                                                     </div>
-                                                    <span className={styles.playerInfoName} style={{ color: playerRoles[0].color }}>PlayerName</span>
+                                                    <span className={styles.playerInfoName} style={{ color: 'var(--color-grass)' }}>{reply.sender.username}</span>
                                                     <span className={styles.ticketStarter}>Starter</span>
                                                 </div>
                                             </div>
@@ -84,7 +88,7 @@ const ViewTicket = () => {
                             <form action="/" method="post">
                                 <textarea placeholder="Write your comment here" name="comment"></textarea>
                                 <div>
-                                    <img src={profilePicture} width="35" alt="Profile of Bradley Jones" title="Bradley Jones" />
+                                    <img src={profilePicture} width="35" alt={username} title={username} /> {username}
                                     <button type="submit">Submit</button>
                                 </div>
                             </form>
